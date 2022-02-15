3b25660222624c9ade569f3935de3ac8519ddb0b
chipsandsalsa1 <shnoelrose12@gmail.com>
2022-02-15 16:15:44 -0500

    starting to write things - broken code for homepage

--- backend/AdminPortal/hp.py
+++ backend/AdminPortal/hp.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, Blueprint
+import pathlib
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-	return render_template("login.html")
+	return "Hello World"
+
 
 if __name__ == "__main__":
 	app.run()
