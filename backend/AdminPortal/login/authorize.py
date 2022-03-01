502a484221507443f0b267b356f54adb532016cf
Michael Anderson <michael.j.anderson@me.com>
2022-02-28 23:33:46 -0500

    Fixed Signup : )

--- backend/AdminPortal/login/authorize.py
+++ backend/AdminPortal/login/authorize.py
@@ -4,12 +4,14 @@ from passlib.context import CryptContext
 from db import db
 
 db = SQLAlchemy()
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+	
 
 def verify(password, Hash):
 	return CryptContext(schemes=["bcrypt"], deprecated="auto").verify(password, Hash)
@@ -23,19 +25,23 @@ class User(db.Model):
 	username = db.Column(db.String(32), unique = True)
 	passwrd = db.Column(db.String(64), unique = True)
 	name = db.Column(db.String(64))
-	#authkey = db.Column(db.String(16), unique = True)
+	authkey = db.Column(db.String(16), unique = True)
 
 
 @app.route("/", methods = ['GET','POST'])
 def register():
 	if request.method == 'POST':
 		
+
 		name = request.form.get('name')
 		email = request.form.get('email')
 		passwrd = request.form.get('passwrd')
 		authkey = request.form.get('authkey')
+		print(name)
+		print(email)
+		print(passwrd)
 
-		newUser = User(id=1, username=email, passwrd=hashPass(passwrd), name=name)
+		newUser = User(username=email, passwrd=hashPass(passwrd), name=name, authkey=authkey)
  
 		db.session.add(newUser)
 		db.session.commit()
@@ -44,7 +50,6 @@ def register():
 		
 	else:
 		return render_template('signup.html')
-
 
 if __name__ == "__main__":
 	app.run(debug=True);
