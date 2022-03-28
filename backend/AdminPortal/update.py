f6c1699e56ab0698b63a56c67dacf3bdeda72ca6
Michael Anderson <michael.j.anderson@me.com>
2022-03-28 00:33:57 -0400

    Pathway Database Manipulation

--- backend/AdminPortal/update.py
+++ backend/AdminPortal/update.py
@@ -7,36 +7,70 @@ from datetime import datetime
 session = boto3.Session(region_name='us-east-1', profile_name='auto')
 db = session.resource('dynamodb')
 
-tab = db.Table('course_database').scan()
+course_data = db.Table('courses').scan()
+pathway_data = db.Table('pathways').scan()
 
-for i in tab['Items']:
+for i in pathway_data['Items']:
+    for j in i:
+        if(type(i[j]) == decimal.Decimal):
+            i[j] = int(i[j])
+        elif(type(i[j]) == list):
+            for k in range(len(i[j])):
+                i[j][k] = int(i[j][k])
+
+for i in course_data['Items']:
     for j in i:
         if(type(i[j]) == decimal.Decimal):
             i[j] = int(i[j])
 
+print(pathway_data['Items'])
+
 courses = []
-for x in range(len(tab['Items'])):
+for x in range(len(course_data['Items'])):
     outerFields = dict()
     outerFields["info"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    outerFields["pk"] = tab['Items'][x]['pk']
+    outerFields["pk"] = course_data['Items'][x]['pk']
     outerFields["model"] = "database.course"
     innerFields = dict()
-    innerFields["prefix"] = tab['Items'][x]['prefix']
-    innerFields["ID"] = tab['Items'][x]['ID'] 
-    innerFields["name"] = tab['Items'][x]['name']
-    innerFields["description"] = tab['Items'][x]['description']
-    innerFields["HI"] = tab['Items'][x]['HI']
-    innerFields["CI"] = tab['Items'][x]['CI']
-    innerFields["DI"] = tab['Items'][x]['DI']
-    innerFields["major_restrictive"] = tab['Items'][x]['major_restrictive']
-    innerFields["fall"] = tab['Items'][x]['fall']
-    innerFields["spring"] = tab['Items'][x]['spring']
-    innerFields["summer"] = tab['Items'][x]['summer']
+    innerFields["prefix"] = course_data['Items'][x]['prefix']
+    innerFields["ID"] = course_data['Items'][x]['ID'] 
+    innerFields["name"] = course_data['Items'][x]['name']
+    innerFields["description"] = course_data['Items'][x]['description']
+    innerFields["HI"] = course_data['Items'][x]['HI']
+    innerFields["CI"] = course_data['Items'][x]['CI']
+    innerFields["DI"] = course_data['Items'][x]['DI']
+    innerFields["major_restrictive"] = course_data['Items'][x]['major_restrictive']
+    innerFields["fall"] = course_data['Items'][x]['fall']
+    innerFields["spring"] = course_data['Items'][x]['spring']
+    innerFields["summer"] = course_data['Items'][x]['summer']
     outerFields["fields"] = innerFields
     courses.append(outerFields)
 
+pathways = []
+for x in range(len(pathway_data['Items'])):
+    outerFields = dict()
+    outerFields["info'"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    outerFields["pk"] = pathway_data['Items'][x]['pk']
+    outerFields["model"] = "database.pathway"
+
+    innerFields = dict()
+    innerFields["pathName"] = pathway_data['Items'][x]['pathName']
+    innerFields["pathDescript"] = pathway_data['Items'][x]['pathDescript']
+    innerFields["priority1"] = pathway_data['Items'][x]['priority1'].copy()
+    innerFields["priority2"] = pathway_data['Items'][x]['priority2'].copy()
+    innerFields["priority3"] = pathway_data['Items'][x]['priority3'].copy()
+
+    outerFields["fields"] = innerFields
+    pathways.append(outerFields)
+
+
+
 print(courses)
 courseDataFile = open("courses.json", 'w')
+pathwayDataFile = open("pathways.json", "w")
 courseDataFile.truncate()
+pathwayDataFile.truncate()
 courseDataFile.write(json.dumps(courses, sort_keys=True, indent=4))
+pathwayDataFile.write(json.dumps(pathways, sort_keys=True, indent=4))
 courseDataFile.close()
+pathwayDataFile.close()
