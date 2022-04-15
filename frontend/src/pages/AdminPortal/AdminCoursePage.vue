c82a87667edcad69676477ad0fe715cef445c10c
Ryzon3 <ryanmcole20@gmail.com>
2022-04-15 17:08:34 -0400

    Updated Admin COurse page

--- frontend/src/pages/AdminPortal/AdminCoursePage.vue
+++ frontend/src/pages/AdminPortal/AdminCoursePage.vue
@@ -8,6 +8,53 @@
                 rounded
                 label="Course Name"
             />
+            <v-text-field
+                v-model="dept"
+                outlined
+                rounded
+                label="Department"
+            />
+            <v-text-field
+                v-model="ID"
+                outlined
+                rounded
+                label="Course ID"
+                type="number"
+            />
+            <v-textarea
+                v-model="description"
+                outlined
+                rounded
+                label="Description"
+            />
+            <v-checkbox 
+                v-model="CI"
+                label="Communication Intensive"
+            />
+            <v-checkbox 
+                v-model="HI"
+                label="Hass Inquiry"
+            />
+            <v-checkbox 
+                v-model="fall"
+                label="Offered in Fall"
+            />
+            <v-checkbox 
+                v-model="summer"
+                label="Offered in Summer"
+            />
+            <v-checkbox 
+                v-model="spring"
+                label="Offered in Spring"
+            />
+            <v-select
+                v-model="myPathways"
+                multiple
+                label="Pathways"
+                outlined
+                chips
+                readonly
+            />
         </v-container>
     </div>
 </template>
@@ -15,7 +62,7 @@
 <script>
 import Breadcrumbs from '../../components/Breadcrumbs'
 import breadcrumbs from '../../data/breadcrumbs.js'
-import { courses } from '../../data/data.js'
+import { courses, pathways } from '../../data/data.js'
 
 export default {
     components: {
@@ -24,15 +71,55 @@ export default {
     data() {
         return {
             breadcrumbs: breadcrumbs.admin_course_page,
-            name: ""
+            name: "",
+            dept: "",
+            ID: 0,
+            CI: false,
+            HI: false,
+            description: "",
+            fall: false,
+            summer: false,
+            spring: false,
+            major_rest: false,
+            minors: [],
+            myPathways: [],
+            pathways: []
         }
     },
     created() {
-        this.name = this.getCourse().name;  
+        const course = this.getCourse();
+        if(course) {
+            this.name = course.name;
+            this.dept =  course.prefix;
+            this.ID = course.ID;
+            this.CI = course.properties.CI;
+            this.HI = course.properties.HI;
+            this.description = course.description;
+            this.fall = course.offered.fall;
+            this.summer = course.offered.summer;
+            this.spring = course.offered.spring;
+            this.major_rest = course.properties.major_restricted;
+        }
+        let myPathways = new Set();
+        for(const key in pathways) {
+            const singlePathway = pathways[key];
+            if(course) {
+                for(const prio in singlePathway) {
+                    if(prio.substring(0, 8) == "priority") {
+                        const array = singlePathway[prio];
+                        if(array.includes(course.name.slice().toLowerCase().replace(/ /g, '_'))) {
+                            myPathways.add(singlePathway.name);
+                        }
+                    }
+                }
+            }
+            this.pathways.push(singlePathway.name);
+        }
+        this.myPathways = Array.from(myPathways);
     },
     computed: {
         foundCrumbs() {
-            let course = this.getCourse();
+            const course = this.getCourse();
             if(course) {
                 return breadcrumbs.pathway_template.map(x => x || {
                     text: course.name,
