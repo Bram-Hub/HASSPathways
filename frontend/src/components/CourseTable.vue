b8165aa20ee2ee95924318d6d5bc275325b6981a
James Liu <liuxjames88@gmail.com>
2022-06-17 16:43:39 -0400

    trying to get conditional classes working

--- frontend/src/components/CourseTable.vue
+++ frontend/src/components/CourseTable.vue
@@ -17,7 +17,7 @@
                 style="width: 400px; max-width: 100%"
             />
         </v-card>
-        <div :class="{container: graphView}">
+        <div>
             <CourseTableCourse
                 v-for="item in filteredCourses"
                 :key="item.name"
@@ -118,8 +118,8 @@ export default {
 
 <style scoped>
 .container {
-    display: grid;
-    grid-template-columns: repeat(2, 50%);
+    /*display: grid;*/
+    /*grid-template-columns: repeat(2, 50%);*/
 }
 .table-header-search {
     transform: scale(0.8);
