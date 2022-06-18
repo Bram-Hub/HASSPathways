fedb4f4314c503695999ee3d75ea05413949e760
Michael Ni <michael.ni678@gmail.com>
2022-06-17 20:06:33 -0400

    changed routerlink to 'to'

--- frontend/src/pages/AdminPortal/AdminHomePage.vue
+++ frontend/src/pages/AdminPortal/AdminHomePage.vue
@@ -4,45 +4,25 @@
             <Breadcrumbs :breadcrumbs="breadcrumbs" />
             <h1>Welcome to the Admin Portal!</h1>
             <p>Here you can edit the courses and pathways on the website!</p>
-
             <v-divider class="my-4" />
-
             <ul id="btn-cntnr">
-                <v-btn id="admn-btn">
-                    <router-link
-                        :to="{ name: 'admin-pathway' }"
-                        class="text-decoration-none"
-                        id="admn-fnt"
-                    >
-                        <v-icon left dark>
-                            mdi-magnify
-                        </v-icon>
-                        Search for a class by pathway
-                    </router-link>
+                <v-btn id="admn-btn" :to="{ name: 'admin-search-course-code' }" class="text-decoration-none">
+                    <v-icon left dark>
+                        mdi-magnify
+                    </v-icon>
+                    Search for a class by pathway
                 </v-btn>
-                <v-btn id="admn-btn">
-                    <router-link
-                        :to="{ name: 'admin-search-course-code' }"
-                        class="text-decoration-none"
-                        id="admn-fnt"
-                    >
-                        <v-icon left dark>
-                            mdi-book-edit
-                        </v-icon>
-                        Edit a class by course code
-                    </router-link>
+                <v-btn id="admn-btn" :to="{ name: 'admin-search-course-code' }" class="text-decoration-none">
+                    <v-icon left dark>
+                        mdi-book-edit
+                    </v-icon>
+                    Edit a class by course code
                 </v-btn>
-                <v-btn id="admn-btn">
-                    <router-link
-                        :to="{ name: 'admin-course' }"
-                        class="text-decoration-none"
-                        id="admn-fnt"
-                    >
-                        <v-icon left dark>
-                            mdi-book-plus
-                        </v-icon>
-                        Add a new course to the website
-                    </router-link>
+                <v-btn id="admn-btn" :to="{ name: 'admin-course' }" class="text-decoration-none">
+                    <v-icon left dark>
+                        mdi-book-plus
+                    </v-icon>
+                    Add a new course to the website
                 </v-btn>
             </ul>
         </v-container>
@@ -68,6 +48,7 @@ export default {
 <style scoped>
 #btn-cntnr {
     padding: 0px !important;
+    text-align: center;
 }
 #admn-btn {
     list-style: none;
@@ -77,9 +58,8 @@ export default {
     margin: 0px 1vw 1vw 0px;
     height: 50px;
     max-width: 900px;
-}
-#admn-fnt {
     color: white;
-    font-size: 15px;
+    font-size: 15px !important;
+    padding-top: 15px;
 }
 </style>
