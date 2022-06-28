c50f7a7e5c96d77232b3b9e3c30607188d502940
james <liuxjames88@gmail.com>
2022-06-28 14:26:15 -0400

    fix #76

--- frontend/src/pages/Pathway/PathwaysPage.vue
+++ frontend/src/pages/Pathway/PathwaysPage.vue
@@ -2,9 +2,6 @@
     <div>
         <v-container>
             <Breadcrumbs :breadcrumbs="breadcrumbs" />
-            <!-- <v-btn class="search" href="/search-classes">
-                Search for a class
-            </v-btn> -->
             <h1>HASS Pathways</h1>
             <p>
                 Select a pathway below to get started, or <router-link to="/search-classes">
