ae8efebe43557ccd9ba98959d58dad8880a2e44d
ACE3R2 <bavrahamics@gmail.com>
2022-06-29 22:01:57 -0400

    Got a basic selection in place

---
+++ frontend/src/components/YearSelection.vue
@@ -0,0 +1,31 @@
+<template>
+    <div style="max-width: 500px; float: right;">
+        <v-select :items="allyears" :label="year" />
+    </div>
+</template>
+
+<script>
+import { pathways } from '../data/data.js'
+
+export default {
+    name: 'YearSelection',
+    data() {
+        return {
+            year: "2021-2022"
+        }
+    },
+    computed: {
+        // allyears() {
+        //     return pathways.keys()
+        // }
+        allyears() {
+            return ["2021-2022","2020-2021","2019-2020","2018-2019","2017-2018"]
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
