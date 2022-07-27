3d4c6eeae4383d95687afc97e7c40c0c00336fbd
Michael Anderson <michael.j.anderson@me.com>
2022-07-27 02:24:29 -0400

    the purge

--- frontend/src/data/vuex.js
+++
@@ -1,7 +0,0 @@
-// Use these instead of hardcoding string keys
-// So you don't make a typo
-export const DARK_MODE = 'darkMode';
-export const PATHWAYS = 'pathways';
-export const CLASSES = 'classes';
-
-export const DEFAULT_DARK_MODE = true;
