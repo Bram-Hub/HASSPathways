a65c5e9929530421c692b22764552915669e8c0a
nicdicosimo <95448450+dicosn@users.noreply.github.com>
2022-07-29 15:19:00 -0400

    Overhaul of dark mode switch
    The code for how it works has changed to be more readable and makes sense for a v-switch

--- frontend/src/components/DarkLightModeButton.vue
+++ frontend/src/components/DarkLightModeButton.vue
@@ -1,47 +1,51 @@
 <template>
-    <v-tooltip v-if="$vuetify.theme.dark" bottom>
-        <template #activator="{off}">
-            <v-switch
-                inset
-                small fab elevation="0"
-                v-on="off"
-                @click="darkMode"
-                size="small"
-                prepend-icon="mdi-moon-waxing-crescent"
-                style="padding-top:21px;"
-            >
+    <v-switch
         
-            </v-switch>
-        </template>
-        <span>Switch to Light Mode</span>
-    </v-tooltip> 
-    <v-tooltip v-else bottom>
-        <template #activator="{on}">
-            <v-switch
-                small fab elevation="0"
-                inset
-                prepend-icon="mdi-white-balance-sunny" 
-                color="yellow darken-2"
-                value="yellow darken-2"
-                size="small"
-                @click="darkMode"
-                style="padding-top:21px;"
-            >
-            </v-switch>
-        </template>
-        <span>Switch to Dark Mode</span>
-    </v-tooltip>
+        :value="$vuetify.theme.dark"
+        small fab elevation="0"
+        inset
+        v-on="off"
+        v-model="t"
+        :prepend-icon="correctIcon"
+                
+        color="yellow darken-2"
+            
+        size="small"
+        @change="darkMode"
+        style="padding-top:21px;"
+    >
+    </v-switch>
+
 </template>
+
 
 <script>
 export default {
     name: 'DarkLightModeButton',
-
+    //:defaultState="true"
+    props: {
+        defaultState: {
+            type: Boolean, 
+            default: false
+        }
+    },
+    computed:{
+        correctIcon(){
+            if(this.$vuetify.theme.dark){
+               return "mdi-moon-waxing-crescent";
+            }
+            else{
+                return "mdi-white-balance-sunny";
+            }
+        }
+    },
     methods: {
         darkMode() {
+            //prepend-icon="mdi-moon-waxing-crescent"
             this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
             this.$store.commit('setDarkMode', this.$vuetify.theme.dark);
         }
+
     }
 }
 </script>
