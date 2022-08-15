00f76b938066ad8fbab5b7d0112178a34ec60b72
Ryzon3 <ryanmcole20@gmail.com>
2022-08-15 18:53:35 -0400

    Uncommented scraper post bug

--- backend/scrapers/main.py
+++ backend/scrapers/main.py
@@ -11,32 +11,32 @@ from tqdm import tqdm
 if __name__ == "__main__":
     years = list(map(lambda x: x[0], courses_scraper.get_catalogs()))
     years = years[:4]
-    # all_courses = courses_scraper.scrape_courses()
+    all_courses = courses_scraper.scrape_courses()
 
-    # print("Started scraping CI courses")
-    # for year in tqdm(years):
-    #     path = '../../frontend/src/data/json/' + str(year)
-    #     try:
-    #         # Make dir if does not already exist
-    #         os.mkdir(path)
-    #     except Exception:
-    #         print(f"Folder for {str(year)} already made")
-    #     f = open(path + '/courses.json', 'w')
-    #     json.dump(all_courses[year], f, sort_keys=True, indent=2, ensure_ascii=False)
-    #     f.close()
-    #     split_years = [year[:4], year[5:]]
-    #     asyncio.run(sis_scraper.scrape_CI(split_years[0], "fall", path + '/courses.json'))
-    #     asyncio.run(sis_scraper.scrape_CI(split_years[1], "spring", path + '/courses.json'))
-    #     asyncio.run(sis_scraper.scrape_CI(split_years[1], "summer", path + '/courses.json'))
-    # print("Finished scraping CI courses")
+    print("Started scraping CI courses")
+    for year in tqdm(years):
+        path = '../../frontend/src/data/json/' + str(year)
+        try:
+            # Make dir if does not already exist
+            os.mkdir(path)
+        except Exception:
+            print(f"Folder for {str(year)} already made")
+        f = open(path + '/courses.json', 'w')
+        json.dump(all_courses[year], f, sort_keys=True, indent=2, ensure_ascii=False)
+        f.close()
+        split_years = [year[:4], year[5:]]
+        asyncio.run(sis_scraper.scrape_CI(split_years[0], "fall", path + '/courses.json'))
+        asyncio.run(sis_scraper.scrape_CI(split_years[1], "spring", path + '/courses.json'))
+        asyncio.run(sis_scraper.scrape_CI(split_years[1], "summer", path + '/courses.json'))
+    print("Finished scraping CI courses")
 
-    # all_pathways = pathway_scraper.scrape_pathways()
-    # for year in years:
-    #     path = '../../frontend/src/data/json/' + str(year)
+    all_pathways = pathway_scraper.scrape_pathways()
+    for year in years:
+        path = '../../frontend/src/data/json/' + str(year)
 
-    #     f = open(path + '/pathways.json', 'w')
-    #     json.dump(all_pathways[year], f, sort_keys=True, indent=2, ensure_ascii=True)
-    #     f.close()
+        f = open(path + '/pathways.json', 'w')
+        json.dump(all_pathways[year], f, sort_keys=True, indent=2, ensure_ascii=True)
+        f.close()
 
     print("Starting to fill non-catalog courses")
     for year in years:
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         asyncio.run(fill_empty.fill(path))
     print("Finished to fill non-catalog courses")
     
-    # f = open('../../frontend/src/data/json/' + 'years.json', 'w');
-    # json.dump(years, f, sort_keys=True, indent=2, ensure_ascii=True)
-    # f.close()
-    # category_assembler.assemble('../../frontend/src/data/json/')
+    f = open('../../frontend/src/data/json/' + 'years.json', 'w');
+    json.dump(years, f, sort_keys=True, indent=2, ensure_ascii=True)
+    f.close()
+    category_assembler.assemble('../../frontend/src/data/json/')
