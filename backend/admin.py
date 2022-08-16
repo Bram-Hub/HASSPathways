71d371501206e6f722460765b6a7768f002ac0c3
Michael Anderson <michael.j.anderson@me.com>
2022-08-16 01:21:45 -0400

    Updating Backend script to match the one running on EC2

--- backend/admin.py
+++ backend/admin.py
@@ -1,24 +1,62 @@
-from flask import Flask, request, json, jsonify
+from flask import Flask, request, json, jsonify, session, redirect, url_for
 from flask_cors import CORS, cross_origin
+from cas import CASClient
+
 app = Flask(__name__)
 c = CORS(app)
 CORS(app, resources={r'/*': {'origins': '*'}},CORS_SUPPORTS_CREDENTIALS = True)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+cas = CASClient(
+        version = 3,
+        service_url='https://ec2-52-90-250-109.compute-1.amazonaws.com/login/rpi',
+        server_url='https://cas.auth.rpi.edu/cas/'
+        )
+
+
+@app.route('/guard')
+def guard(method=['GET']):
+        if 'username' in session:
+                return jsonify('{"auth": "1"}')
+        else:
+                return jsonify('{"auth": "0"}')
+
+@app.route('/admin')
+def index():
+        return redirect(url_for('login'))
+
+@app.route('/login/rpi', methods=["POST", "GET"])
+def login():
+        #print(next)
+        ticket = request.args.get('ticket')
+        if not ticket:
+                print(cas.get_login_url())
+                return redirect(cas.get_login_url())
+
+        print(ticket)
+        user, attributes, pgtiou = cas.verify_ticket(ticket)
+
+        if not user:
+                return "Failed to Verify Login Ticket"
+        else:
+                session['username'] = user
+                return redirect("https://hasspathways.com/admin-portal")
+
+
 
 @app.route("/edit", methods=["POST", "GET"])
 def editAdmin():
-	response = {'status':'success'}
-	if request.method == "POST":
-		dat = request.get_json()
-		name = dat.get('courses'),
-		pathways = dat.get('pathways')
-		print(name)
-		print(pathways)
+        response = {'status':'success'}
+        if request.method == "POST":
+                dat = request.get_json()
+                name = dat.get('courses'),
+                pathways = dat.get('pathways')
+                print(name)
+                print(pathways)
 
-		response['message'] = 'Success!'
+                response['message'] = 'Success!'
 
-	return jsonify(response)
+        return jsonify(response)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+        app.run(host='0.0.0.0')
