f75ec8e6efe3cf1f7bdad3d6e7c7428c113bc8d4
chengyuzz <chengyuz0921@gmail.com>
2022-10-18 16:09:33 -0400

    Update CoursePage.vue

--- frontend/src/pages/Courses/CoursePage.vue
+++ frontend/src/pages/Courses/CoursePage.vue
@@ -5,6 +5,7 @@
             <h1>{{ course.subj }}-{{ course.ID }}: {{ course.name }}</h1>
         </div>
         <p>{{ course.description }}</p>
+        <h3>Professor:</h3>
         <h3>Available: {{ course.offered.text }}</h3>
         <br>
         <h3>Prerequisites:</h3>
