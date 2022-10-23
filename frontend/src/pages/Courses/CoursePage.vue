1bfb75db9294365c1d5912022f4ba381eb78f917
chengyuzz <chengyuz0921@gmail.com>
2022-10-23 14:58:44 -0400

    Add a function to correspond professors and their own sections
    This commit mainly change two things:
    [1] Add a function to correspond professors and their own sections
    [2] Arrange the sections a certain professor teaches just below their name

--- frontend/src/pages/Courses/CoursePage.vue
+++ frontend/src/pages/Courses/CoursePage.vue
@@ -8,23 +8,28 @@
         <p>{{ course.description }}</p>
         <h3>Instructor(s) name:</h3>
         <h4>Rate my Professor link:</h4>
-        <template>
-        <v-expansion-panels>
-            <v-expansion-panel
-            v-for="(item,i) in 1"
-            :key="i"
-            >
-            <v-expansion-panel-header>
-                Sections
-            </v-expansion-panel-header>
-            <v-expansion-panel-content>
-                <button>  SectionNum-CRN | StartDate-EndDate </button> <br>
-                SectionNum-CRN | StartDate-EndDate <br>
-                SectionNum-CRN | StartDate-EndDate <br>
-                SectionNum-CRN | StartDate-EndDate <br>
-            </v-expansion-panel-content>
-            </v-expansion-panel>
-        </v-expansion-panels>
+        
+        <template v-if="course.professors.length !== 0">
+            <h3>Professors:</h3>
+            <ul>
+                <li v-for="pro in course.professors" :key="pro.id">
+                    {{ pro }}
+                    <template>
+                        <h5>Section information:</h5>
+                        <ul>
+                            <li v-for="sec in course.sections" :key="sec.id">
+                                    <h5>Section ID: {{ sec.id }}</h5>
+                                    <h5>Section Type: {{ sec.type }}</h5>
+                                    <h5>Section Time: {{ sec.time }}</h5>
+                                    <h5>Section Location: {{ sec.location }}</h5>
+                                    <h5>Section Days: {{ sec.days }}</h5>
+                                    <h5>Section Instructor: {{ sec.instructor }}</h5>
+                            </li>
+                        </ul>
+                    </template>
+                </li>
+            </ul>
+            <h3> </h3>
         </template>
         <template v-else>
             <h3>This class is not providing in the current semester</h3>
@@ -42,6 +47,7 @@ import { courseCategories } from '../../data/data.js'
 import Breadcrumbs from '../../components/Breadcrumbs'
 import breadcrumbs from '../../data/breadcrumbs.js'
 import CourseTableModifiers from '../../components/CourseTableModifiers'
+
 
 export default {
     components: {
@@ -77,8 +83,25 @@ export default {
                 text: this.course.name, 
                 href: 'course?course=' + encodeURIComponent(this.courseID)
             })
-        }
-    },
+        },
+
+        // Correspond Professors and their own section(s)
+        profSec() {
+            let profSec = [];
+            for (let i = 0; i < this.course.professors.length(); ++i){
+                let sec = [];
+                let prof = this.course.professors[i];
+                for (let j = 0; j < this.course.sections.length(); ++j)
+                {
+                    if (this.course.sections[j].instructor === prof){
+                        sec.push(this.course.sections[j]);
+                    }
+                }
+                profSec.push({professor: prof, sections: sec});
+            }
+            return profSec;
+        },
+
     created() {
         const year = this.$store.state.year;
         import('../../data/json/' + year + '/courses.json').then((val) => {this.coursesData = Object.freeze(val);
@@ -88,6 +111,7 @@ export default {
             }
         });
     }
+}
 }
 </script>
 
