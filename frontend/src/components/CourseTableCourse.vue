119c79ee342197cdc73a1ac72e0bfc4ef22b6493
kesfc <kevinli20030305@gmail.com>
2022-11-03 18:15:42 -0400

    fixed a bug
    fixed bug when people in pathways and try go to the course page, when they select the "course link", the page will select the class for them in the pathway.
    And i move the course link under it

--- frontend/src/components/CourseTableCourse.vue
+++ frontend/src/components/CourseTableCourse.vue
@@ -108,13 +108,16 @@
                 class="class-card__desc"
             >
                 {{ course.description }}
+                <p></p>
                 <div v-if="course.professors.length != 0">
                     Taught by:
                     <li v-for="(prof, index) in course.professors" :key="prof" style="list-style: none; display: inline;">
                         {{ prof }}{{ index &lt; course.professors.length - 1 ? ", " : "" }}
                     </li>
                 </div>
-                <a :href="`/course?course=${encodeURIComponent(course.name)}`"> Course Link </a>
+                <div v-else>
+                    Taught by: Unknown
+                </div>
             </v-card-text>
             <v-card-text
                 v-if="course.ID == null"
@@ -123,6 +126,12 @@
                 Data not found within RPI catalog, see SIS for more info.
             </v-card-text>
         </v-card>
+        <v-card-text
+            v-if="course.ID != null"
+            >
+            <a :href="`/course?course=${encodeURIComponent(course.name)}`"> Course Link</a>
+            &nbsp; for {{ course.name }}
+            </v-card-text>
     </div>
 </template>
 
