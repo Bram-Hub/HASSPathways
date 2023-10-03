29cef7515fc2001deaa9742a3e004674f706f765
Daniel Na <element11sodium@gmail.com>
2023-10-03 10:11:49 -0400

    fixing catalog scraping

--- backend/scrapers/courses_scraper.py
+++ backend/scrapers/courses_scraper.py
@@ -15,7 +15,7 @@ import csv
 BASE_URL = "http://rpi.apis.acalog.com/v1/"
 # It is ok to publish this key because I found it online already public
 DEFAULT_QUERY_PARAMS = "?key=3eef8a28f26fb2bcc514e6f1938929a1f9317628&format=xml"
-CHUNK_SIZE = 500
+CHUNK_SIZE = 100
 
 # returns the list of catalogs with the newest one being first
 # each catalog is a tuple (year, catalog_id) ex: ('2020-2021', 21)
